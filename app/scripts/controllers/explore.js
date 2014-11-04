79072d6d32a5d7f8e05e759ebddfd8b1477b2400
Robin W <wrobin@gmx.net>
2014-11-04 15:01:41 +0100

    adds stylized bar chart directive with data from user

--- app/scripts/controllers/explore.js
+++ app/scripts/controllers/explore.js
@@ -11,12 +11,14 @@ angular.module('offrApp')
   .controller('ExploreCtrl', ['$scope', 'User', function($scope, User) {
 	User.query(function(data){
 	    $scope.users = data;
-	    $scope.selectedUser = data[0];      
+	    $scope.selectedUser = data[0];  
+	    $scope.hardskill = data[0].hardskill;   
 	}, function(err){
 	    console.log('request failed');
 	});
 
   $scope.changeSelectedUser = function(user) {
     $scope.selectedUser = user;
+    $scope.hardskill = user.hardskill;  
   }
 }]);
--- app/scripts/directives/tagbarchart.js
+++ app/scripts/directives/tagbarchart.js
@@ -7,80 +7,122 @@
  * # tagBarChart
  */
 angular.module('offrApp')
-  .directive('simpleLineChart', ['d3Service', function(d3Service) {
+  .directive('tagBarChart', ['d3Service', function(d3Service) {
     return {
       restrict: 'EA',
-      scope: {},
+      scope: { 
+      	hardskill: '='
+      },
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
 
-          var margin = {top: 20, right: 20, bottom: 30, left: 50},
-            width = 600 - margin.left - margin.right,
-            height = 200 - margin.top - margin.bottom;
-
-          var parseDate = d3.time.format('%d-%b-%y').parse;
-
-          var x = d3.time.scale()
-            .range([0, width]);
-
-          var y = d3.scale.linear()
-            .range([height, 0]);
-
-          var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient('bottom');
-
-          var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('left');
-
-          var line = d3.svg.line()
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.close); });
-
-          var svg = d3.select(element[0]).append('svg')
-           .attr('width', width + margin.left + margin.right)
-           .attr('height', height + margin.top + margin.bottom)
-           .append('g')
-           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
           // Hard coded data
-          scope.data = [
-            {date: '4-Apr-12', close: 34},
-            {date: '5-Apr-12', close: 45},
-            {date: '6-Apr-12', close: 37},
-            {date: '7-Apr-12', close: 56},
-            {date: '8-Apr-12', close: 50},
-            {date: '9-Apr-12', close: 77}
-          ];
+          /*scope.data = [
+            {tag: 'JavaScript', expertise: 34},
+            {tag: 'D3', expertise: 45},
+            {tag: 'Angular', expertise: 37},
+            {tag: 'Ember', expertise: 56},
+            {tag: 'Backbone', expertise: 50},
+            {tag: 'Grunt', expertise: 77}
+          ];*/
+          
 
-          scope.data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
+          scope.hardskill.forEach(function(d) {
+            d.expertise = +d.expertise;
           });
 
-          x.domain(d3.extent(scope.data, function(d) { return d.date; }));
-          y.domain(d3.extent(scope.data, function(d) { return d.close; }));
+          var margin = {top: 40, right: 20, bottom: 100, left: 40},
+				    width = 500 - margin.left - margin.right,
+				    height = 300 - margin.top - margin.bottom;
 
-          svg.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(xAxis);
+					var formatPercent = d3.format("%");
 
-          svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis)
-            .append('text')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', 6)
-            .attr('dy', '.71em')
-            .style('text-anchor', 'end')
-            .text('Price ($)');
+					var x = d3.scale.ordinal()
+					    .rangeRoundBands([0, width], .1);
 
-          svg.append('path')
-            .datum(scope.data)
-            .attr('class', 'line')
-            .attr('d', line);
-        });
-      }};
-    }]);
+					var y = d3.scale.linear()
+					    .range([height, 0]);
+
+					var xAxis = d3.svg.axis()
+					    .scale(x)
+					    .orient("bottom");
+
+					var yAxis = d3.svg.axis()
+					    .scale(y)
+					    .tickFormat("")
+					    .ticks("")
+					    .orient("left");
+
+					var svg = d3.select(element[0]).append('svg')
+					    .attr("width", width + margin.left + margin.right)
+					    .attr("height", height + margin.top + margin.bottom)
+					  .append("g")
+					    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+				  x.domain(scope.hardskill.map(function(d) { return d.tag; }));
+				  y.domain([0, d3.max(scope.hardskill, function(d) { return d.expertise; })]);
+
+				  svg.append("g")
+				      .attr("class", "x axis")
+				      .attr("transform", "translate(0," + height + ")")
+				      .call(xAxis)
+           	.selectAll("text")  
+	            .style("text-anchor", "end")
+	            .attr("dx", "-.8em")
+	            .attr("dy", ".15em")
+	            .attr("transform", function(d) {
+	                return "rotate(-65)" 
+              });
+
+				  var yAxis = svg.append("g")
+				      .attr("class", "y axis")
+				      .call(yAxis);
+				  
+				  // append y axis labels
+			    yAxis.append("text")
+			    	.attr("class", "label-mid")
+			      .attr("transform", "rotate(-90)")
+			      .attr("x", -height/2)
+			      .attr("y", -38)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "middle")
+			      .text("Expertise");
+
+  			  yAxis.append("text")
+			      .attr("class", "label-small")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", -22)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("great");
+
+				  yAxis.append("text")
+			      .attr("class", "label-small")
+			      .attr("transform", "rotate(-90)")
+			      .attr("x", -height)
+			      .attr("y", -22)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "start")
+			      .text("poor");
+
+				  svg.selectAll(".bar")
+				      .data(scope.hardskill)
+				    .enter().append("rect")
+				      .attr("class", "bar")
+				      .attr("x", function(d) { return x(d.tag); })
+				      .attr("width", x.rangeBand())
+				      .attr("y", function(d) { return y(d.expertise); })
+				      .attr("height", function(d) { return height - y(d.expertise); })
+			      .on("mouseover", function() {
+			        d3.select(this)
+			          .style("fill", "orangered")
+						}).on("mouseout", function() {
+					    d3.select(this)
+					      .transition()
+					      .duration(250)
+					      .style("fill", "orange");
+						});
+	      });
+	    }
+	  };
+	}]);
