a373f82541337ad0c664589635636a8264a0d0a3
Robin W <wrobin@gmx.net>
2014-11-06 10:47:56 +0100

    adds unfinished test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('offrApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
--- test/spec/controllers/explore.js
+++ test/spec/controllers/explore.js
@@ -6,17 +6,78 @@ describe('Controller: ExploreCtrl', function () {
   beforeEach(module('offrApp'));
 
   var ExploreCtrl,
-    scope;
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('users/users.json').
+        respond([
+          { 
+              "id": 0, 
+              "imageUrl": "images/placeholder_user.jpg", 
+              "name": "Juri Jar", 
+              "description": "Lorem ipsum",
+              "hardskill": [
+                  {"tag": "JavaScript", "expertise": 34},
+                  {"tag": "D3", "expertise": 45},
+                  {"tag": "Angular", "expertise": 37},
+                  {"tag": "Ember", "expertise": 56},
+                ]
+          },  
+          { 
+              "id": 1, 
+              "imageUrl": "images/placeholder_user.jpg", 
+              "name": "Gari Gur", 
+              "description": "Lila laune",
+              "hardskill": [
+                  {"tag": "JavaScript", "expertise": 54},
+                  {"tag": "D3", "expertise": 25},
+                  {"tag": "Angular", "expertise": 67},
+                ]
+          }
+    ]);
+
     scope = $rootScope.$new();
     ExploreCtrl = $controller('ExploreCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    //expect(scope.awesomeThings.length).toBe(3);
+  it('should have a nulled hovered variable', function () {
+    expect(scope.hovered).toBe(null);
+  });
+
+  it('should have a user list', function () {
+    expect(scope.users).toEqual([]);
+    $httpBackend.flush();
+
+    expect(scope.users).toEqual([
+        { 
+            "id": 0, 
+            "imageUrl": "images/placeholder_user.jpg", 
+            "name": "Juri Jar", 
+            "description": "Lorem ipsum",
+            "hardskill": [
+                {"tag": "JavaScript", "expertise": 34},
+                {"tag": "D3", "expertise": 45},
+                {"tag": "Angular", "expertise": 37},
+                {"tag": "Ember", "expertise": 56},
+              ]
+        },  
+        { 
+            "id": 1, 
+            "imageUrl": "images/placeholder_user.jpg", 
+            "name": "Gari Gur", 
+            "description": "Lila laune",
+            "hardskill": [
+                {"tag": "JavaScript", "expertise": 54},
+                {"tag": "D3", "expertise": 25},
+                {"tag": "Angular", "expertise": 67},
+              ]
+        }
+    ]);
+
   });
 });
